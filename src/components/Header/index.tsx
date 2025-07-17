89026d090388a4a7b576b121bb98fd0bdb8f8a3c
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2025-07-17 20:26:27 -0300

    refactor Header component and add styled HeaderContainer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,13 @@
+import { HeaderContainer } from "./style";
+
 export function Header() {
   return (
-    <header className="w-full bg-white shadow p-4 text-center text-xl font-bold">
-      OrganizaDoc
-    </header>
+    <HeaderContainer className="w-full bg-white shadow p-4 text-center">
+      <span className="text-xl font-bold">OrganizaDoc</span>
+      <form>
+        <input type="file" name="sendDocument" id="" />
+        <button type="submit">Enviar Documento</button>
+      </form>
+    </HeaderContainer>
   );
 }
---
+++ src/components/Header/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background: var(-color-text-primary);
+  display: flex;
+  justify-content: space-between;
+  padding: 1.25rem;
+  align-items: center;
+`

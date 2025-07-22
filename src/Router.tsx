c1d02071e85febe45392a8fe0a54adeca0a06ae9
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2025-07-22 18:43:47 -0300

    feat: enhance AppSidebar with categories and new category button

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import { LoginForm } from './pages/LoginForm'
 import { RegisterForm } from './pages/RegisterForm'
 import { DefaultLayout } from './layout/DefaultLayout'
@@ -9,13 +9,14 @@ export function Router() {
   return(
     <Routes>
       <Route path='/' element={<LoginRegisterLayout/>}>
-        <Route path='/' element={<LoginForm/>} />
+        <Route path='/' element={<Navigate to="/login" replace={true}/>} />
         <Route path='/login' element={<LoginForm/>} />
         <Route path='register' element={<RegisterForm/>} />
       </Route>
 
       <Route path='/app' element={<DefaultLayout/>}>
         <Route path='home' element={<Home />} />
+        <Route path='categorie/:categorie'></Route>
       </Route>
     </Routes>
   )
--- src/components/Sidebar/AppSidebar.tsx
+++ src/components/Sidebar/AppSidebar.tsx
@@ -3,15 +3,65 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
 } from "@/components/ui/sidebar"
+
+import { AddressBook, CreditCard, Files, Plus, Receipt, } from 'phosphor-react'
+
+import { NavLink } from "react-router-dom"
+
+const categories = [
+  {
+    title: 'Contratos',
+    url: 'contratos',
+    icon: AddressBook
+  },
+  {
+    title: 'Faturas',
+    url: 'faturas',
+    icon: CreditCard
+  },
+  {
+    title: 'Boletos',
+    url: 'boletos',
+    icon: Receipt
+  },
+  {
+    title: 'Documentos',
+    url: 'documentos',
+    icon: Files
+  }  
+]
 
 export function AppSidebar() {
   return (
     <Sidebar >
-      <SidebarHeader />
+      <SidebarHeader className="h-14 p-0 m-0">
+      <button className="w-full h-full flex justify-center text-center p-4 gap-2.5 cursor-pointer">
+        <Plus size={24} />
+        <span className="font-bold">Nova categoria</span>
+      </button>
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
+          <SidebarGroupLabel>Categorias de Documentos</SidebarGroupLabel>
+          <SidebarMenu>
+            {categories.map((category) => (
+              <SidebarMenuItem key={category.title}>
+                <SidebarMenuButton asChild>
+                  <NavLink to={category.url}>
+                    <category.icon size={24}/>
+                    <span>{category.title}</span>
+                  </NavLink>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter />

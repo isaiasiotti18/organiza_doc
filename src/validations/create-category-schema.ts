34033854139d3cadc34a3e9cca70f536c29fa324
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2025-10-29 18:14:24 -0300

    fix: trim whitespace in category name validation schema

--- src/validations/create-category-schema.ts
+++ src/validations/create-category-schema.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const createCategorySchema = z.object({
-  name: z.string("Input está vazio").min(5, "Minimo 5 caracteres"),
+  name: z.string("Input está vazio").trim().min(5, "Minimo 5 caracteres"),
 });
 
 export type CreateCategorySchema = z.infer<typeof createCategorySchema>;
